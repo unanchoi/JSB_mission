60998512b6c65c24a47d7b2cc49739d613d3966e
unanchoi <unusualc@likelion.org>
2022-03-08 20:18:13 +0900

    Feat : add constructor, relation annotation

--- src/main/java/com/misson/mission3/entity/Board.java
+++ src/main/java/com/misson/mission3/entity/Board.java
@@ -4,18 +4,35 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "board")
-@Getter@Setter
+@Getter
+@Setter
 public class Board {
 
-    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
 
+    @OneToMany(
+            targetEntity = Post.class,
+            fetch = FetchType.LAZY,
+            mappedBy = "board"
+    )
+    @Column(name = "board_name")
+    private List<Post> post = new ArrayList<>();
 
-
-
+    @Override
+    public String toString() {
+        return "Board{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", post=" + post +
+                '}';
+    }
 }
--- src/main/java/com/misson/mission3/entity/Post.java
+++ src/main/java/com/misson/mission3/entity/Post.java
@@ -15,7 +15,30 @@ public class Post {
     private String writer;
     private String content;
 
+    @ManyToOne(
+            targetEntity = Board.class,
+            fetch = FetchType.LAZY
+    )
+    @JoinColumn(name = "board_id")
+    private Board board;
 
+    public Post(Long id, String writer, String content, Board board) {
+        this.id = id;
+        this.writer = writer;
+        this.content = content;
+        this.board = board;
+    }
 
+    public Post() {
+    }
 
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", writer='" + writer + '\'' +
+                ", content='" + content + '\'' +
+                ", board=" + board +
+                '}';
+    }
 }
