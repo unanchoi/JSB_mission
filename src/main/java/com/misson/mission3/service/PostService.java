15dbb72e44d4dbcd7086acafe47f67e99bf93a31
unanchoi <unusualc@likelion.org>
2022-03-19 01:04:26 +0900

    Logging

--- src/main/java/com/misson/mission3/service/PostService.java
+++ src/main/java/com/misson/mission3/service/PostService.java
@@ -4,19 +4,22 @@ import com.misson.mission3.PostDao;
 import com.misson.mission3.entity.Post;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public class PostSerivice {
+public class PostService {
 
-    private static final Logger logger = LoggerFactory.getLogger(PostSerivice.class);
+    private static final Logger logger = LoggerFactory.getLogger(PostService.class);
     private final PostDao postDao;
 
-    public PostSerivice(PostDao postDao) {
+    public PostService(PostDao postDao) {
         this.postDao = postDao;
     }
 
@@ -59,7 +62,11 @@ public class PostSerivice {
     }
 
     public void deletePost(Long id) {
-        this.postDao.deletePost(id);
+        Optional<Post> targetEntity = this.postRepository.findById((long) id);
+        if (targetEntity.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        this.postRepository.delete(targetEntity.get())
     }
 
 
